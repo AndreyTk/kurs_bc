81eae6411f97d6a043014ebbbe24b5a20e114f7d
AndreyTk <andriy.tkachuk@algorithm-plus.com>
2023-11-10 22:26:03 +0200

    less 19 1

--- less_19/token_ANT.sol
+++ less_19/token_ANT.sol
@@ -8,6 +8,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import {VestingWallet} from "@openzeppelin/contracts/finance/VestingWallet.sol";
 
 contract ANTToken is ERC20Burnable, Ownable, ERC20Capped{
     uint8 constant DECIMALS=8;
@@ -29,5 +30,15 @@ contract ANTToken is ERC20Burnable, Ownable, ERC20Capped{
 
     function _update(address _account, address _to, uint256 value) internal override(ERC20,ERC20Capped){
         super._update(_account,_to,value);
+    }
+}
+
+contract ANTTokenVesting is VestingWallet {
+    address beneficiary = 0x76594609F9b5858e6E53c3E5D18b76863dDF8FD3;
+    uint64 startTimestamp = uint64(block.timestamp);
+    uint64 durationSeconds = uint64(60*60*24*30*7);
+
+    constructor() VestingWallet(beneficiary, startTimestamp, durationSeconds){
+
     }
 }
